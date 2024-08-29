48b0dcf02093f495328bbf3f7dd0a79012ba8e45
tranhaduy204 <tranhaduy204@gmail.com>
2024-08-29 16:04:11 +0700

    Api message

--- server/index.js
+++ server/index.js
@@ -4,16 +4,18 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const userRoute = require('./routes/userRoute');
-const chatRoute = require('./routes/chatRoute');
+const userRoute = require("./routes/userRoute");
+const chatRoute = require("./routes/chatRoute");
+const messageRoute = require("./routes/messageRoute");
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/users', userRoute);
-app.use('/api/chats', chatRoute);
+app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
